cc6bd1424ad39a40dd173034890f147e17e83fc9
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-11-16 17:40:45 -0300

    minesweeper app material created

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,5 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+import 'screens/minesweeper_app.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: Container(),
-    );
-  }
-}
+void main() => runApp(MinesweeperApp());
