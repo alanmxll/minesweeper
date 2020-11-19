c452f3c26053e0ba8a81bc2bcd4800a00289e16d
Alan Maxwell <alanmaxwellantunes@gmail.com>
2020-11-19 18:46:54 -0300

    component result

--- lib/screens/minesweeper_app.dart
+++ lib/screens/minesweeper_app.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
+import '../components/result_widget.dart';
+
 class MinesweeperApp extends StatelessWidget {
+  void _restart() {
+    print('Restarted');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Minesweeper'),
+        appBar: ResultWidget(
+          won: true,
+          onRestart: () => _restart(),
         ),
         body: Container(
           child: Text('Board'),
